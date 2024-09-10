6612e2230656f5418ae107748bb7751123cc90c0
ashharabdullah <ashharabdullah328@gmail.com>
2024-09-10 11:21:21 +0500

    portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -2,11 +2,7 @@
 import React from 'react'
 import Image from "next/image"
 import { motion } from 'framer-motion'
-import Capture from "../assets/Capture.png"
-import Capture1 from "../assets/Capture1.png"
-import Capture2 from "../assets/Capture2.png"
-import Capture3 from "../assets/Capture3.png"
-import Capture4 from "../assets/Capture4.png"
+
 const projects = [
     {
         title:"Promtopia",
@@ -14,23 +10,23 @@ const projects = [
         devStack:"Next JS, Node JS, MongoDB and Tailwind CSS",
         link:"https://lnkd.in/dWuhZhNc",
         git:"https://lnkd.in/dfzsha7m",
-        src:Capture,
+        src:"/assets/Capture.png",
     },
     {
         title:"Gamify",
-        desc:"A  website built with React and Material UI!  . Its designed with three key sections: a left bar for navigation, a feed for dynamic content, and a right bar for additional information. This project highlights my skills in creating responsive, user-friendly interfaces",
+        desc:"A website built with React and Material UI!  Its designed with three key sections: a left bar for navigation, a feed for dynamic content, and a right bar for additional information. This project highlights my skills in creating responsive, user-friendly interfaces.",
         devStack:"React JS and Material UI",
         link:"https://lnkd.in/dzbvfUuP",
         git:"https://lnkd.in/dS5746gK",
-        src:Capture1,
+        src:"/assets/Capture1.png",
     },
     {
         title:"Nexcent",
-        desc:"This project involved implementing React Router for seamless navigation and handling various UI components. The hands-on experience allowed me to deepen my understanding of React's core principles and routing capabilities. In this I use Bootstrap for styling, ensuring a responsive and professional layout.",
+        desc:"This project involved implementing React Router for seamless navigation and handling various UI components. The hands-on experience allowed me to deepen my understanding of React's core principles and routing capabilities. In this, I used Bootstrap for styling, ensuring a responsive and professional layout.",
         devStack:"Figma, React JS and Bootstrap",
         link:"https://lnkd.in/d-f9hMpp",
         git:"https://lnkd.in/dAuU82P3",
-        src:Capture2,
+        src:"/assets/Capture2.png",
     },
     {
         title:"Manage",
@@ -38,32 +34,33 @@ const projects = [
         devStack:"HTML, CSS, JS and Tailwind CSS",
         link:"",
         git:"",
-        src:Capture3,
+        src:"/assets/Capture3.png",
     },
     {
         title:"Bootcamp",
-        desc:"I create a fully responsive bootcamp website with the power of bootstrap. it was a great experience for me to build this project",
-    
+        desc:"I created a fully responsive bootcamp website with the power of Bootstrap. It was a great experience for me to build this project.",
         devStack:"HTML, CSS and Bootstrap",
         link:"https://lnkd.in/dQHjkR7i",
         git:"https://lnkd.in/dYKZDchZ",
-        src:Capture4,
+        src:"/assets/Capture4.png",
     }
 ]
+
 const Portfolio = () => {
   return (
     <div className="text-white bg-gradient-to-b from-black to-[#381a5f] py-18 mt-52" id="portfolio">
-               <h1 className="text-orange-400  text-6xl max-w-[320px] mx-auto font-semibold my-12  ">
-                Port<span className="w-[0] text-white">folio</span> </h1>
-        <div className="px-6 md:px-0 max-w-[1000px] mx-auto  mt-40 space-y-24">
+        <h1 className="text-orange-400 text-6xl max-w-[320px] mx-auto font-semibold my-12">
+            Port<span className="w-[0] text-white">folio</span>
+        </h1>
+        <div className="px-6 md:px-0 max-w-[1000px] mx-auto mt-40 space-y-24">
         {projects.map((project, index) => (
             <motion.div
-            key={index}
-            initial={{opacity: 0, y: 75}}
-            whileInView={{opacity: 1, y: 0}}
-            viewport={{once: true}}
-            transition={{ duration: 0.5, delay: 0.25}}
-            className={`mt-12 flex flex-col ${index % 2 == 1 ? " md:flex-row-reverse gap-12" : "flex-col md:flex-row"}`}
+                key={index}
+                initial={{opacity: 0, y: 75}}
+                whileInView={{opacity: 1, y: 0}}
+                viewport={{once: true}}
+                transition={{ duration: 0.5, delay: 0.25}}
+                className={`mt-12 flex flex-col ${index % 2 === 1 ? "md:flex-row-reverse gap-12" : "flex-col md:flex-row"}`}
             > 
                 <div className="space-y-2 max-w-[550px]">
                     <h2 className="text-7xl my-4 text-white/70">{`0${index + 1}`}</h2>
@@ -77,15 +74,17 @@ const Portfolio = () => {
                 </div>
 
                 <div className='flex justify-center items-center'>
-                    <Image src={project.src} alt={project.title} className="h-[350px] w-auto object-cover border rounded border-gray-700"/> 
-
+                    <Image 
+                      src={project.src} 
+                      alt={project.title} 
+                      width={550} 
+                      height={350} 
+                      className="h-[350px] w-auto object-cover border rounded border-gray-700"
+                    /> 
                 </div>
-
             </motion.div>
-        ) )}
+        ))}
         </div>
-
-
     </div>
   )
 }
